0f3011349663bcc527ea63760a76389b37567af1
Hyeonyeong <105722709+CreateGabi@users.noreply.github.com>
2022-10-24 17:25:34 +0900

    Add files via upload

---
+++ day01/src/day0601/실제링크드리스트.java
@@ -0,0 +1,12 @@
+package day0601;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class 실제링크드리스트 {
+
+	public static void main(String[] args) {
+		List list = new LinkedList();
+	}
+
+}
